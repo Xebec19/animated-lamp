3029d5a1345db81de972895835160f6843eeed40
xebec19 <rohant946@gmail.com>
2022-06-24 07:40:39 +0530

    wip layouts

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -2,12 +2,16 @@ package main
 
 import (
 	"animated-lamp/pkg/handlers"
+	"fmt"
 	"net/http"
 )
+
+const portNumber = ":8080"
 
 func main() {
 	http.HandleFunc("/", handlers.Home)
 	http.HandleFunc("/about", handlers.About)
 	http.HandleFunc("/temp", handlers.Divide)
-	_ = http.ListenAndServe(":8080", nil)
+	fmt.Printf(fmt.Sprintf("Starting application on port %s", portNumber))
+	_ = http.ListenAndServe(portNumber, nil)
 }
--- pkg/render/render.go
+++ pkg/render/render.go
@@ -3,14 +3,48 @@ package render
 import (
 	"fmt"
 	"net/http"
+	"path/filepath"
 	"text/template"
 )
 
+var functions = template.FuncMap{}
+
 func RenderTemplate(w http.ResponseWriter, tmpl string) {
+	_, err := RenderTemplateTest(w)
+	if err != nil {
+		fmt.Println("Error getting template cache:", err)
+	}
 	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
 	err := parsedTemplate.Execute(w, nil)
 	if err != nil {
 		fmt.Println("Error parsing template:", err)
 		return
 	}
+}
+
+func RenderTemplateTest(w http.ResponseWriter, tmpl string) (map[string]*template.Template, error) {
+	myCache := map[string]*template.Template{}
+	pages, err := filepath.Glob("./templates/*.page.tmpl")
+	if err != nil {
+		return myCache, err
+	}
+	for _, page := range pages {
+		name := filepath.Base(page)
+		ts, err := template.New(name).Funcs(functions).ParseFiles(page)
+		if err != nil {
+			return myCache, err
+		}
+		matches, err := filepath.Glob("./templates/*.layout.tmpl")
+		if err != nil {
+			return myCache, err
+		}
+		if len(matches) > 0 {
+			ts, err = ts.ParseGlob("./templates/*.layout.tmpl")
+			if err != nil {
+				return myCache, err
+			}
+		}
+		myCache[name] = ts
+	}
+	return myCache, nil
 }
